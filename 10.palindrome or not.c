5e95607307dd7f7c620b4e1511bffdc6d1011c57
varshasivakuma <112751877+varshasivakuma@users.noreply.github.com>
2024-03-27 00:41:37 -0700

    Update 10.palindrome or not.c

--- 10.palindrome or not.c
+++ 10.palindrome or not.c
@@ -4,22 +4,20 @@
 int main() {
     char str[100];
     int i, length;
-    int isPalindrome = 1; // Assume palindrome initially
+    int isPalindrome = 1;
     
     printf("Enter a string: ");
     scanf("%s", str);
     
     length = strlen(str);
     
-    // Check for palindrome
     for (i = 0; i < length / 2; i++) {
         if (str[i] != str[length - i - 1]) {
-            isPalindrome = 0; // Not a palindrome
+            isPalindrome = 0; 
             break;
         }
     }
     
-    // Output result
     if (isPalindrome)
         printf("%s is a palindrome.\n", str);
     else
