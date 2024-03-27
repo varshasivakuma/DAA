3c122c4ba04741857cdd208fbeb24557007f0043
varshasivakuma <112751877+varshasivakuma@users.noreply.github.com>
2024-03-27 00:43:53 -0700

    Update 15..strassen matrix mul.c

--- 15..strassen matrix mul.c
+++ 15..strassen matrix mul.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-#define N 4 // Size of matrices
+#define N 4 
 
 void strassen(int A[N][N], int B[N][N], int C[N][N]) {
-    // Base case: if the matrix is 1x1
+    
     if (N == 1) {
         C[0][0] = A[0][0] * B[0][0];
         return;
     }
 
-    // Dividing the matrices into submatrices
     int a[N/2][N/2], b[N/2][N/2], c[N/2][N/2], d[N/2][N/2];
     int e[N/2][N/2], f[N/2][N/2], g[N/2][N/2], h[N/2][N/2];
     int temp1[N/2][N/2], temp2[N/2][N/2];
 
-    // Dividing matrix A into 4 submatrices
     for (int i = 0; i < N/2; i++) {
         for (int j = 0; j < N/2; j++) {
             a[i][j] = A[i][j];
@@ -24,7 +22,6 @@ void strassen(int A[N][N], int B[N][N], int C[N][N]) {
         }
     }
 
-    // Dividing matrix B into 4 submatrices
     for (int i = 0; i < N/2; i++) {
         for (int j = 0; j < N/2; j++) {
             e[i][j] = B[i][j];
@@ -34,7 +31,6 @@ void strassen(int A[N][N], int B[N][N], int C[N][N]) {
         }
     }
 
-    // Calculating the 7 intermediate matrices recursively
     strassen(a, e, temp1);
     strassen(b, g, temp2);
     for (int i = 0; i < N/2; i++) {
@@ -88,8 +84,7 @@ int main() {
                     {25, 26, 27, 28},
                     {29, 30, 31, 32} };
 
-    int C[N][N]; // Result matrix
-
+    int C[N][N]; 
     strassen(A, B, C);
 
     printf("Result matrix C:\n");
