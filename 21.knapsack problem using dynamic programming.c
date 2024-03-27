7c524d5505f8bfd41ad7dcde60bc7416db872ec9
varshasivakuma <112751877+varshasivakuma@users.noreply.github.com>
2024-03-27 01:08:17 -0700

    Update 21.knapsack problem using dynamic programming.c

--- 21.knapsack problem using dynamic programming.c
+++ 21.knapsack problem using dynamic programming.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
-
-// Function to find maximum of two integers
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
-
-// Knapsack function without using functions
 int main() {
     int i, j;
-    int n;  // Number of items
-    int capacity;  // Capacity of the knapsack
+    int n; 
+    int capacity; 
     printf("Enter the number of items: ");
     scanf("%d", &n);
     printf("Enter the capacity of the knapsack: ");
@@ -21,8 +17,6 @@ int main() {
         printf("Item %d: ", i);
         scanf("%d %d", &weight[i], &value[i]);
     }
-
-    // Initializing the DP table
     int dp[n+1][capacity+1];
     for (i = 0; i <= n; i++) {
         for (j = 0; j <= capacity; j++) {
@@ -34,8 +28,6 @@ int main() {
                 dp[i][j] = dp[i-1][j];
         }
     }
-
-    // Finding the items included in the knapsack
     int includedItems[n+1];
     i = n;
     j = capacity;
@@ -48,15 +40,11 @@ int main() {
         }
         i--;
     }
-
-    // Displaying the items included in the knapsack
     printf("\nItems included in the knapsack are:\n");
     for (i = 1; i <= n; i++) {
         if (includedItems[i])
             printf("Item %d\n", i);
     }
-
-    // Displaying the maximum value that can be obtained
     printf("\nMaximum value that can be obtained: %d\n", dp[n][capacity]);
 
     return 0;
